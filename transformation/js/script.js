d8ae8fc6ec535718e49b2f1d8e142b77a0daf5f7
layla-scruggs <167109626+layla-scruggs@users.noreply.github.com>
2025-04-13 01:11:32 -0400

    Add files via upload

---
+++ transformation/js/script.js
@@ -0,0 +1,28 @@
+
+
+$(document).ready(function(){
+    $(".bee").click(function(){
+        $("#basket").append( "<li>bee</li>" );
+    });
+
+    $(".ladybug").click(function(){
+        $("#basket").append( "<li>ladybug</li>" );
+    });
+
+    $(".butterfly").click(function(){
+        $("#basket").append( "<li>butterfly</li>" );
+    });
+
+    $(".snail").click(function(){
+        $("#basket").append( "<li>snail</li>" );
+    });
+
+    $(".caterpillar").click(function(){
+        $("#basket").append( "<li>caterpillar</li>" );
+    });
+
+    $(".dragonflies").click(function(){
+        $("#basket").append( "<li>dragonflies</li>" );
+    });
+
+  });
